a153768aa069fd7fa037b664d699709228a1c391
Hugo Heyman <hugo.heyman@gmail.com>
2012-09-12 16:47:28 +0200

    made schedule_once and schedule_interval local functions inside schedule; merged redundant if statement

--- soon.py
+++ soon.py
@@ -7,13 +7,19 @@ class Soon(object):
         self.register = {}
          
     def schedule(self, func, *args, **kw_args):
+    
+        def schedule_interval(func, name, interval, *args, **kw_args):
+            gevent.spawn(func, *args, **kw_args)
+            self.register[name] = gevent.spawn_later(interval, schedule_interval, func, name, interval, *args, **kw_args)
+        
+        def schedule_once(func, delay, name):
+            gevent.sleep(delay)
+            func()
+            del self.register[name]
+    
         name = kw_args.pop("name", None)
         start = kw_args.pop("start", None)
         interval = kw_args.pop("interval", None)
-        
-        if interval is not None:
-            if isinstance(interval, dict):
-                interval = interval.get("days", 0) * 86400 + interval.get("hours", 0) * 3600 + interval.get("minutes", 0) * 60 + interval.get("seconds", 0)
         
         if isinstance(start, datetime):
             delay = (start - datetime.now()).total_seconds()
@@ -26,19 +32,15 @@ class Soon(object):
             name = func.__name__
             
         if interval is not None:
-            self.register[name] = gevent.spawn_later(delay, self.schedule_interval, func, name, interval, *args, **kw_args)
-        else:
-            self.register[name] = gevent.spawn(self.schedule_once, func, delay, name)
+        
+            if isinstance(interval, dict):
+                interval = interval.get("days", 0) * 86400 + interval.get("hours", 0) * 3600 + interval.get("minutes", 0) * 60 + interval.get("seconds", 0)
+                
+            self.register[name] = gevent.spawn_later(delay, schedule_interval, func, name, interval, *args, **kw_args)
             
-    def schedule_interval(self, func, name, interval, *args, **kw_args):
-        gevent.spawn(func, *args, **kw_args)
-        self.register[name] = gevent.spawn_later(interval, self.schedule_interval, func, name, interval, *args, **kw_args)
-    
-    def schedule_once(self, func, delay, name):
-        gevent.sleep(delay)
-        func()
-        del self.register[name]
-    
+        else:
+            self.register[name] = gevent.spawn(schedule_once, func, delay, name)
+            
     def unschedule(self, name):
         if name in self.register:
             gevent.kill(self.register[name])
@@ -50,4 +52,4 @@ class Soon(object):
         
     def list(self):
         return [name for name in self.register]
-            
+    
