89d9a9f8f42073503ae1739cf087f9bc578bc50d
Hugo Heyman <hugo.heyman@gmail.com>
2012-09-18 15:18:05 +0200

    There was no need for soon be be a class; fixed a bug where delay could be set to a dict

--- soon.py
+++ soon.py
@@ -1,55 +1,59 @@
 import gevent
+from gevent import monkey
 from datetime import datetime
 
-class Soon(object):
+monkey.patch_all()
 
-    def __init__(self):
-        self.register = {}
+__all__ = ("schedule", "unschedule", "clear", "list")
+
+_register = {}
          
-    def schedule(self, func, *args, **kw_args):
+def schedule(func, *args, **kw_args):
+
+    def schedule_interval(func, name, interval, *args, **kw_args):
+        gevent.spawn(func, *args, **kw_args)
+        _register[name] = gevent.spawn_later(interval, schedule_interval, func, name, interval, *args, **kw_args)
     
-        def schedule_interval(func, name, interval, *args, **kw_args):
-            gevent.spawn(func, *args, **kw_args)
-            self.register[name] = gevent.spawn_later(interval, schedule_interval, func, name, interval, *args, **kw_args)
-        
-        def schedule_once(func, delay, name, *args, **kw_args):
-            gevent.sleep(delay)
-            func(*args, **kw_args)
-            del self.register[name]
+    def schedule_once(func, delay, name, *args, **kw_args):
+        gevent.sleep(delay)
+        func(*args, **kw_args)
+        del _register[name]
+
+    name = kw_args.pop("name", None)
+    start = kw_args.pop("start", None)
+    interval = kw_args.pop("interval", None)
     
-        name = kw_args.pop("name", None)
-        start = kw_args.pop("start", None)
-        interval = kw_args.pop("interval", None)
+    if isinstance(start, datetime):
+        delay = (start - datetime.now()).total_seconds()
+    elif isinstance(start, dict):
+        delay = start.get("days", 0) * 86400 + start.get("hours", 0) * 3600 + start.get("minutes", 0) * 60 + start.get("seconds", 0)
+    else:
+        delay = start
         
-        if isinstance(start, datetime):
-            delay = (start - datetime.now()).total_seconds()
-        elif isinstance(start, dict):
-            delay = start.get("days", 0) * 86400 + start.get("hours", 0) * 3600 + start.get("minutes", 0) * 60 + start.get("seconds", 0)
-        else:
+    if name is None:
+        name = func.__name__
+        
+    if interval is not None:
+    
+        if isinstance(interval, dict):
+            interval = interval.get("days", 0) * 86400 + interval.get("hours", 0) * 3600 + interval.get("minutes", 0) * 60 + interval.get("seconds", 0)
+            
+        if delay is None:
             delay = interval
-            
-        if name is None:
-            name = func.__name__
-            
-        if interval is not None:
+
+        _register[name] = gevent.spawn_later(delay, schedule_interval, func, name, interval, *args, **kw_args)
         
-            if isinstance(interval, dict):
-                interval = interval.get("days", 0) * 86400 + interval.get("hours", 0) * 3600 + interval.get("minutes", 0) * 60 + interval.get("seconds", 0)
-                
-            self.register[name] = gevent.spawn_later(delay, schedule_interval, func, name, interval, *args, **kw_args)
-            
-        else:
-            self.register[name] = gevent.spawn(schedule_once, func, delay, name, *args, **kw_args)
-            
-    def unschedule(self, name):
-        if name in self.register:
-            gevent.kill(self.register[name])
-            del self.register[name]
-    
-    def clear(self):
-        for key in register:
-            self.unschedule(key)
+    else:
+        _register[name] = gevent.spawn(schedule_once, func, delay, name, *args, **kw_args)
         
-    def list(self):
-        return [name for name in self.register]
+def unschedule(name):
+    if name in _register:
+        gevent.kill(_register[name])
+        del _register[name]
+
+def clear():
+    for key in _register:
+        unschedule(key)
     
+def list():
+    return [name for name in _register]
